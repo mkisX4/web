de762b6d7d2583b55108b218e793a50325e62492
Pingwinshik <nnikmats@gmail.com>
2022-12-07 17:48:04 +0300

    V0.2
    Now with output!

--- Pages/Queue.cshtml.cs
+++ Pages/Queue.cshtml.cs
@@ -1,20 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Web.Pages.Shared
+namespace Web.Pages
 {
     public class QueueModel : PageModel
     {
-        private readonly ILogger<QueueModel> _logger;
-
-        public QueueModel(ILogger<QueueModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
-
         }
     }
 }
